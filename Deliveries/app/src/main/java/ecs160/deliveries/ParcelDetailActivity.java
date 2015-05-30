18e6042559a48a60fbfe7832de8f1d0d0a1a720e
Andrew Green <eatmorehippo@gmail.com>
2015-05-29 19:31:20 -0700

    Nothing magical in this change. Just adding some basics for the parcel detail activity. It should work once we implement some way to grab the parcel off the database from just its ID.

--- Deliveries/app/src/main/java/ecs160/deliveries/ParcelDetailActivity.java
+++ Deliveries/app/src/main/java/ecs160/deliveries/ParcelDetailActivity.java
@@ -6,6 +6,13 @@ import android.support.v7.app.ActionBarActivity;
 
 import android.view.MenuItem;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * An activity representing a single Parcel detail screen. This
@@ -47,5 +54,61 @@ public class ParcelDetailActivity extends ActionBarActivity {
                     .add(R.id.parcel_detail_container, fragment)
                     .commit();
         }
+    }
+
+
+
+    /*Added by Andrew
+    Parcel details is pretty simple right now. Just shows the parcel view and that's about it.
+    */
+    TextView description;
+    TextView source;
+    TextView destination;
+    TextView carrier;
+    TextView place;
+    TextView time;
+    Button finalize_button;
+
+    int pID; //parcel ID
+    int mUID; //my user ID
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        //Setup Getters
+        description = (TextView) findViewById(R.id.TextParcelDescription);
+        source = (TextView) findViewById(R.id.TextParcelSource);
+        destination = (TextView) findViewById(R.id.TextParcelDestination);
+        carrier = (TextView) findViewById(R.id.TextParcelCarrier);
+        place = (TextView) findViewById(R.id.TextParcelPlace);
+        time = (TextView) findViewById(R.id.TextParcelTime);
+        finalize_button = (Button) findViewById(R.id.ButtonParcelFinalize);
+
+        mUID = getIntent().getIntExtra("uid", -1);
+        pID = getIntent().getIntExtra("item_id", -1);
+
+        //Initialize buttons
+        finalize_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        API.finalize(pID, mUID);
+                    }
+                }
+        );
+
+        //Get parcel information
+        //API.parcel(this, "ParcelCallback", pID);
+    }
+
+    public void ParcelCallback(Object ret) throws JSONException{
+        JSONObject my_parcel = ((JSONArray) ret).getJSONObject(0);
+        description.setText(my_parcel.getString("description"));
+        source.setText(my_parcel.getString("source"));
+        destination.setText(my_parcel.getString("destination"));
+        carrier.setText(my_parcel.getString("carrier"));
+        //place.setText(my_parcel.getString("lat")); //TODO: figure out what you want to store here (just lat/lng?)
+        time.setText(my_parcel.getString("time"));
     }
 }
--- Deliveries/app/src/main/java/ecs160/deliveries/UserDetailActivity.java
+++ Deliveries/app/src/main/java/ecs160/deliveries/UserDetailActivity.java
@@ -3,14 +3,14 @@ package ecs160.deliveries;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
+import android.widget.Button;
+import android.widget.TextView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import android.view.MenuItem;
 
