b9f89d8cc938a7000863283bd5078548f135c0dd
Andrew Green <eatmorehippo@gmail.com>
2015-06-04 01:25:46 -0700

    Updated to fix up the UI for the parcel detail. Some problems, check the TODO and you'll see a few of the issues I've come up against. Most of the TODOs have to do with using text fields rather than date/time things. One in particular that requires a choice I'd like your input on is, should the create parcel create the parcel without a location/time and just start in state 1, or should it be sent out immediately? If it's immediate I need the parcel API to return the ID used by the parcel. Otherwise we can probably get away with just creating the parcel in the state 0 (unsent request).

--- Deliveries/app/src/main/java/ecs160/deliveries/ParcelDetailActivity.java
+++ Deliveries/app/src/main/java/ecs160/deliveries/ParcelDetailActivity.java
@@ -33,6 +33,7 @@ public class ParcelDetailActivity extends ActionBarActivity {
     EditText description_edit;
     EditText source_edit;
     EditText destination_edit;
+    EditText courier_edit;
     EditText carrier_edit;
     EditText location_edit;
     EditText time_edit;
@@ -41,21 +42,21 @@ public class ParcelDetailActivity extends ActionBarActivity {
     Button send_request_button;
     Button accept_request_button;
     Button decline_request_button;
+    Button create_parcel_button;
 
     int pID; //parcel ID
     int mUID; //my user ID
+    boolean is_creating_parcel;
 
     @Override
-    public void onStart(){
+    public void onStart() {
         super.onStart();
-
-        //TODO: Get an intent that returns whether or not we are creating a new parcel or just editing/viewing an old one
-        boolean is_creating_parcel = false;
 
         //Setup Getters
         description_edit = (EditText) findViewById(R.id.EditParcelDescription);
         source_edit = (EditText) findViewById(R.id.EditParcelSource);
         destination_edit = (EditText) findViewById(R.id.EditParcelDestination);
+        courier_edit = (EditText) findViewById(R.id.EditParcelCourier);
         carrier_edit = (EditText) findViewById(R.id.EditParcelCarrier);
         location_edit = (EditText) findViewById(R.id.EditParcelLocation);
         time_edit = (EditText) findViewById(R.id.EditParcelTime);
@@ -65,9 +66,12 @@ public class ParcelDetailActivity extends ActionBarActivity {
         send_request_button = (Button) findViewById(R.id.ButtonParcelSendRequest);
         accept_request_button = (Button) findViewById(R.id.ButtonParcelAcceptRequest);
         decline_request_button = (Button) findViewById(R.id.ButtonParcelDeclineRequest);
+        create_parcel_button = (Button) findViewById(R.id.ButtonParcelCreateParcel);
 
+        //Setup intents
         mUID = getIntent().getIntExtra("uid", -1);
-        pID = getIntent().getIntExtra("item_id", -1);
+        pID = getIntent().getIntExtra("id", -1); //TODO: Right now this is returning -1
+        is_creating_parcel = false; //TODO: Get an intent that returns whether or not we are creating a new parcel or just editing/viewing an old one
 
         //Initialize buttons
         finalize_button.setOnClickListener(
@@ -106,28 +110,51 @@ public class ParcelDetailActivity extends ActionBarActivity {
                 }
         );
 
-        //Get parcel information
-        API.parcel(this, "parcelCallback", pID);
+        create_parcel_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        createParcel();
+                    }
+                }
+        );
+
+        if (is_creating_parcel) {
+
+            create_parcel_button.setVisibility(View.VISIBLE);
+
+            description_edit.setEnabled(true);
+            source_edit.setEnabled(true);
+            destination_edit.setEnabled(true);
+            courier_edit.setEnabled(true);
+            carrier_edit.setEnabled(true);
+            location_edit.setEnabled(true);
+            time_edit.setEnabled(true);
+            date_edit.setEnabled(true);
+        } else {
+            //Get parcel information
+            API.parcel(this, "parcelCallback", pID);
+        }
     }
 
     //Callback for the API, returns JSON for parcel
     //Example JSON: [{"id":"1","description":"test1","status":"0","source":"20","destination":"24","courier":"0","carrier":"20","lat":"0","lng":"0","time":"0"}]
-    public void parcelCallback(Object ret) throws JSONException{
+    public void parcelCallback(Object ret) throws JSONException {
         //Grab parcel and set the values for all the text edits
         //TODO: probably have to change the way we set information once fancy widgets are in
         JSONObject my_parcel = ((JSONArray) ret).getJSONObject(0);
-        description_edit.setText("Description: " + my_parcel.getString("description"));
-        source_edit.setText("Source: " + my_parcel.getString("source"));
-        destination_edit.setText("Destination: " + my_parcel.getString("destination"));
-        carrier_edit.setText("Carrier: " + my_parcel.getString("carrier"));
-        location_edit.setText("Lat:" + my_parcel.getString("lat") + "Lng: " + my_parcel.getString("lng"));
-        time_edit.setText("Time: " + my_parcel.getString("time"));
+        description_edit.setText(my_parcel.getString("description"));
+        source_edit.setText(my_parcel.getString("source"));
+        destination_edit.setText(my_parcel.getString("destination"));
+        courier_edit.setText(my_parcel.getString("courier"));
+        carrier_edit.setText(my_parcel.getString("carrier"));
+        location_edit.setText(my_parcel.getString("lat"));
+        time_edit.setText(my_parcel.getString("time"));
         //date_edit.setText("Time: " + my_parcel.getString("time")); //TODO: fill this in once we've got the date conversion working
 
         //Setup UI based on status
         String p_status = my_parcel.getString("status");
-        switch(p_status)
-        {
+        switch (p_status) {
             //0 and 3 are the same
             case "0": //TODO: Make sure that this works, I assume that Java doesn't stop until a break
             case "3":
@@ -164,8 +191,28 @@ public class ParcelDetailActivity extends ActionBarActivity {
     }
 
     //Called when the user wants to respond to another users request
-    public void requestResponse(boolean is_accept){
+    public void requestResponse(boolean is_accept) {
+        API.rendezvousResponse(pID, is_accept);
         accept_request_button.setEnabled(false);
         decline_request_button.setEnabled(false);
+    }
+
+    //Called when the user is happy with their parcel they've created
+    //Creates the parcel and creates a request at the same time
+    public void createParcel() {
+        int send_destination = Integer.parseInt(destination_edit.toString());
+        int send_time = Integer.parseInt(time_edit.toString());
+        double send_lat = 0;
+        double send_lng = 0;
+        boolean send_courier = courier_edit.toString().length() > 0; //TODO: replace text element with checkbox
+        API.addParcel(this, "createParcelCallback", mUID, send_destination, description_edit.toString(),
+                send_lat, send_lng, send_time, send_courier);
+    }
+
+    //TODO: implement this fully
+    public void createParcelCallback(){
+        //created parcel yay
+        //probably return to main menu at this point
+        //could return the pid and create the response right now, otherwise it could start off in state 0 and not require date/time immediately
     }
 }
